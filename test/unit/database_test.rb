f882c009bb614abaea3b87e5690304990e438284
Jacek Panachida <jacek.panachida@gmail.com>
2008-06-24 23:53:17 +0200

    Add database tests.

--- test/unit/database_test.rb
+++ test/unit/database_test.rb
@@ -3,9 +3,15 @@ require File.dirname( __FILE__ ) + '/../test_helper'
 
 class DatabaseTest < ActiveSupport::TestCase
 
+  self.use_transactional_fixtures = false
+  
   fixtures :databases,
            :drivers
 
+  def setup
+    @db_sqlite = databases( :sqlite3 )
+  end
+  
   def test_invalid
     db = Database.new
     assert !db.valid?
@@ -33,5 +39,28 @@ class DatabaseTest < ActiveSupport::TestCase
     db = databases( :oracle )
     assert db.valid?
   end
-
+  
+  def test_has_table
+    tables = @db_sqlite.tables
+    tables.each { |t| assert @db_sqlite.has_table?(t.name) }
+  end
+  
+  def test_has_no_existing_table
+    assert !@db_sqlite.has_table?('x_not_existing_table_x')
+  end
+  
+  def test_get_table
+    tables = @db_sqlite.tables
+    tables.each do |t|
+      table = @db_sqlite.get_table(t.name)
+      assert_equal t.name, table.name
+      assert_equal t.driver, table.driver
+      assert_equal t.database, table.database
+    end
+  end
+  
+  def test_get_no_existing_table
+    assert_nil @db_sqlite.get_table('x_not_existing_table_x')
+  end
+ 
 end
